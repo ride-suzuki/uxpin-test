c7152e28e2fe519f0828af9a1163fd4300623dd9
鈴木智 <hiromu@hiromu-MBP.local>
2024-03-18 22:31:32 +0900

    first commit

---
+++ uxpin.webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    modules: [__dirname, 'node_modules'],
+    extensions: ['*', '.tsx']
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.tsx$/,
+        exclude: /node_modules/
+      },
+      {
+        loader: 'ts-loader',
+        test: /\.tsx$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2
+            },
+          },
+        ],
+      },
+    ]
+  }
+}
